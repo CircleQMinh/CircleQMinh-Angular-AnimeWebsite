4002b6cb4cf4eda5fab0ec5fcf8c8cf7606d7c37
CircleQMinh <quocminh.vutran3105@gmail.com>
2021-07-27 15:46:18 +0700

    2707

--- angular-frontend/src/app/component/home/home.component.ts
+++ angular-frontend/src/app/component/home/home.component.ts
@@ -9,61 +9,166 @@ import { InfomationService } from 'src/app/service/infomation.service';
 })
 export class HomeComponent implements OnInit {
 
-  data:any[]=[]
-  pageSize=12
+  dataNewest: any[] = []
+  pageSizeNewest = 12
+  pageNewest: number = 0
+  newestCount: number = 0
+  newestItem: any[] = []
 
-  pageNewest:number=0
-  newestCol:number=0
-  newestItem:any[]=[]
-  constructor(private infoService:InfomationService) { }
+  dataUpComing: any[] = []
+  upComingItem: any[] = []
+  pageUpComing: number = 0
+  pageSizeUpComing = 7
 
-  ngOnInit(): void {
-    this.infoService.getAnimeByYearAndSeason("2021","summer").subscribe(
-      data=>{
-        console.log(data.anime)
-        this.data=data.anime
+  dataNewpaper: any[] = []
+  newSoure: any[] = [44511, 38000, 37999, 1, 2]
+  newItems: any[] = []
+  pageNewpaper = 0
+  pageNewpaperCount = 0
+  pageNewPaperSize = 4
+
+  constructor(private infoService: InfomationService) { }
+
+  async ngOnInit(): Promise<void> {
+    this.infoService.getAnimeByYearAndSeason("2021", "summer").subscribe(
+      data => {
+        this.dataNewest = data.anime
         this.getNewest()
       },
-      error=>{
+      error => {
+        console.log(error)
+      }
+    )
+    this.infoService.getTopUpCommingAnime().subscribe(
+      data => {
+        //console.log(data.top)
+        this.dataUpComing = data.top
+        this.getUpComing()
+      },
+      error => {
+        console.log(error)
+      }
+    )
+    this.newSoure.forEach(element => {
+      this.getAnimeNew(element)
+      console.log(element)
+      console.log(this.dataNewpaper)
+      this.getAnimeNewPage()
+      console.log(this.newItems)
+    });
+
+  }
+  getNewest() {
+    this.newestItem = []
+    for (let i = 0; i < this.pageSizeNewest; i++) {
+      this.newestItem.push(this.dataNewest[i + this.pageSizeNewest * this.pageNewest])
+    }
+  }
+  getUpComing() {
+    this.upComingItem = []
+    for (let i = 0; i < this.pageSizeUpComing; i++) {
+      this.upComingItem.push(this.dataUpComing[i + this.pageSizeUpComing * this.pageUpComing])
+    }
+  }
+
+  prevUpComing() {
+    if (this.pageUpComing > 0) {
+      this.pageUpComing -= 1
+      this.getUpComing()
+    }
+  }
+
+  nextUpComing() {
+    if (this.pageUpComing < 6) {
+      this.pageUpComing += 1
+      this.getUpComing()
+    }
+  }
+  async getAnimeNew(num: number) {
+    this.infoService.getAnimeNews(num).subscribe(
+      data => {
+        let a: any[] = []
+        a = data.articles
+        a.forEach(element => {
+          this.dataNewpaper.push(element)
+        });
+      },
+      error => {
         console.log(error)
       }
     )
   }
-  getNewest(){
-    this.newestItem=[]
-    for(let i=0;i<this.pageSize;i++){
-      this.newestItem.push(this.data[i+this.pageSize*this.pageNewest])
+
+  getAnimeNewPage() {
+    this.newItems = []
+    for (let i = 0; i < this.pageNewPaperSize; i++) {
+      this.newItems.push(this.dataNewpaper[i + this.pageNewPaperSize * this.pageNewpaper])
     }
   }
+
+  debug() {
+    console.log("newItems")
+    console.log(this.newItems)
+    console.log("dataNewPapaer")
+    console.log(this.dataNewpaper)
+    this.getAnimeNewPage()
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   images = ['https://pbs.twimg.com/media/EV4xVijWoAASlxY.jpg',
-  'https://zingtv-photo.zadn.vn/tv/1/8/7/2/1872a312fb96c74eb24241b22ad30b18.jpg',
-'https://www.mooreschools.com/cms/lib/OK01000367/Centricity/Domain/5471/Anime%20Banner.png'];
+    'https://zingtv-photo.zadn.vn/tv/1/8/7/2/1872a312fb96c74eb24241b22ad30b18.jpg',
+    'https://www.mooreschools.com/cms/lib/OK01000367/Centricity/Domain/5471/Anime%20Banner.png'];
 
-paused = false;
-unpauseOnArrow = false;
-pauseOnIndicator = false;
-pauseOnHover = true;
-pauseOnFocus = true;
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
+  pauseOnHover = true;
+  pauseOnFocus = true;
 
-togglePaused() {
-  if (this.paused) {
-    this.carousel.cycle();
-  } else {
-    this.carousel.pause();
+  togglePaused() {
+    if (this.paused) {
+      this.carousel.cycle();
+    } else {
+      this.carousel.pause();
+    }
+    this.paused = !this.paused;
   }
-  this.paused = !this.paused;
-}
 
-@ViewChild('carousel', { static: true })
-carousel!: NgbCarousel;
-onSlide(slideEvent: NgbSlideEvent) {
-  if (this.unpauseOnArrow && slideEvent.paused &&
-    (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)) {
-    this.togglePaused();
+  @ViewChild('carousel', { static: true })
+  carousel!: NgbCarousel;
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (this.unpauseOnArrow && slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)) {
+      this.togglePaused();
+    }
+    if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+      this.togglePaused();
+    }
   }
-  if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
-    this.togglePaused();
-  }
-}
 
 }
