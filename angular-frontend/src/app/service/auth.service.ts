69b275770820342efba5cb8ab009a56d2e0a49c7
CircleQMinh <quocminh.vutran3105@gmail.com>
2021-08-07 05:17:48 +0700

    0708

--- angular-frontend/src/app/service/auth.service.ts
+++ angular-frontend/src/app/service/auth.service.ts
@@ -14,8 +14,10 @@ export class AuthService {
   fb_recover="https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key="
   firebaseUrl:string="https://random-website-7f4cf-default-rtdb.firebaseio.com/"
   isLogin:boolean=false
+  
   idLogin:string=""
   emailLogin:string=""
+  userLogin:string=""
 
   constructor(private http: HttpClient,private route:Router) { }
 
@@ -43,12 +45,27 @@ export class AuthService {
     return this.http.post(`${this.firebaseUrl}user.json`,postData)
   }
 
-  getUserInfo(id:number):Observable<any>{
-    return this.http.get(`${this.firebaseUrl}user/${id}`)
+  getUserInfo(id:string):Observable<any>{
+    return this.http.get(`${this.firebaseUrl}user.json?orderBy="id"&equalTo="${id}"&print=pretty`)
   }
   
   getUserInfoByUsername(un:string):Observable<any>{
     return this.http.get(`${this.firebaseUrl}user.json?orderBy="username"&equalTo="${un}"&print=pretty`)
   }
 
+
+  postAnimeComment(anime_id:number,username:string,content:string,rating:number,date:string):Observable<any>{
+    return this.http.post(`${this.firebaseUrl}comment/anime/${anime_id}.json`,{
+      username:username,
+      content:content,
+      rating:rating,
+      date:date})
+  }
+  postMangaComment(manga_id:number,username:string,content:string,rating:number,date:string):Observable<any>{
+    return this.http.post(`${this.firebaseUrl}comment/manga/${manga_id}.json`,{
+      username:username,
+      content:content,
+      rating:rating,
+      date:date})
+  }
 }
